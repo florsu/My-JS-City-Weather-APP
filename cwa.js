23e4c9e5c96a32a3ad5a22d75cf59f33ca1c5c53
Christian Flores <christiangflores0824@gmail.com>
2021-08-26 02:22:10 -0700

    add current & future weather data e.g. icons, color

--- cwa.js
+++ cwa.js
@@ -4,6 +4,7 @@ const DateTime = luxon.DateTime
 const openWeatherapi = '166a433c57516f51dfab1f7edaed8413'
 const currentWeatherURL = 'https://api.openweathermap.org/data/2.5/weather?'
 const uvIndexURL = 'https://api.openweathermap.org/data/2.5/onecall?'
+const iconURL = 'http://openweathermap.org/img/wn/'
 
 
 /**
@@ -34,6 +35,7 @@ function buildUVindexURL(lat, lon) {
         'lon': lon,
         'units': 'imperial',
     }
+
     // Logging the URL so we have access to it for troubleshooting
     console.log("---------------\nURL: " + uvIndexURL + "\n---------------")
     console.log(uvIndexURL + $.param(queryParams))
@@ -41,6 +43,10 @@ function buildUVindexURL(lat, lon) {
 
 }
 
+function buildIconURL(iconID) {
+    return `${iconURL}${iconID}@2x.png`
+    // http://openweathermap.org/img/wn/10d@2x.png
+}
 /* 
 <div style="background: ghostwhite; 
 font-size: 20px; 
@@ -73,15 +79,16 @@ $("#search-button").on("click", function (event) {
  * @param {object} weatherData - object containing Yelp Fusion API data
  */
 function updateCurrentWeather(weatherData) {
+    let date = DateTime.fromSeconds(weatherData.dt).toLocaleString(DateTime.DATE_SHORT)
 
     // Log the weatherData to console, where it will show up as an object
     console.log(weatherData)
     console.log("------------------------------------")
-    $("#city").text(`${weatherData.name} ${weatherData.dt}`)
+    $("#city").text(`${weatherData.name} ${date}`)
     $("#temperature").html(`Temperature: ${weatherData.main.temp} &deg;F`)
     $("#humidity").html(`Humidity: ${weatherData.main.humidity} %`)
     $("#wind_speed").html(`Wind Speed: ${weatherData.wind.speed} MPH`)
-
+    $("#icon").attr('src', buildIconURL(weatherData.weather[0].icon))
     // Build the query URL for the ajax request to the API
     let uvIndexURL = buildUVindexURL(weatherData.coord.lat, weatherData.coord.lon)
 
@@ -99,18 +106,33 @@ function updateCurrentWeather(weatherData) {
 * @param {object} weatherData - object containing API data
 */
 function updateUVindex(weatherData) {
-
+    let uvi = weatherData.current.uvi
+     
     // Log the weatherData to console, where it will show up as an object
     console.log(weatherData)
     console.log("------------------------------------")
-    $("#uv_index").html(`UV Index: ${weatherData.current.uvi}`)
+    $("#uv_index").html(`UV Index: &nbsp;<div id="uv_color">&nbsp;${uvi}&nbsp;</div>`)
+    if (uvi < 3) {
+        $("#uv_color").attr('style', 'background:green;')
+    } else if (uvi < 8) {
+        $("#uv_color").attr('style', 'background:yellow;')
+    } else {
+        $("#uv_color").attr('style', 'background:red;')
+    }
+
     for (let i = 1; i <= 5; i++) {
         updateForecast(i, weatherData.daily[i])
     }
 }
 function updateForecast(i, forecast) {
+    console.log(forecast)
+    console.log(".........................................")
     let date = DateTime.fromSeconds(forecast.dt).toLocaleString(DateTime.DATE_SHORT)
     $(`#date${i}`).text(date)
+    $(`#icon${i}`).attr('src', buildIconURL(forecast.weather[0].icon))
+    $(`#temp${i}`).html(`Temperature: ${forecast.temp.day} &deg;F`)
+    $(`#humidity${i}`).html(`Humidity: ${forecast.humidity} %`)
+
 
 
 }
